2ca7eecb91bd9080e6d0a0dfd2d4b73deadd4b3e
littleFootDev <thelittlefootdev@gmail.com>
2026-03-14 18:45:58 +0100

    feat: add application layer - DTOs, interface and service

---
+++ src/LittleFootCook.Application/DTOs/CreateRecipeDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LittleFootCook.Application.DTOs
+{
+    public record CreateRecipeDto
+    (
+        string Title,
+        string Description,
+        string Difficulty,
+        int PreparationTimeValue,
+        string PreparationTimeUnit,
+        int CookingTimeValue,
+        string CookingTimeUnit,
+        Guid CategoryId
+    );
+}
---
+++ src/LittleFootCook.Application/Interfaces/IRecipeRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using LittleFootCook.Application.DTOs;
+
+namespace LittleFootCook.Application.Interfaces
+{
+    public interface IRecipeRepository
+    {
+        Task<RecipeDto?> GetByIdAsync(Guid id);
+        Task<IEnumerable<RecipeDto>> GetAllAsync();
+        Task<RecipeDto> CreateAsync(CreateRecipeDto dto);
+        Task DeleteAsync(Guid id);
+    }
+}
